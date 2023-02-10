7b62f8cd4f5ecec1619e8bd5d34f5b259978257d
Kirill <Mrgrenn@gmail.com>
2023-02-10 21:18:02 +0300

    done task3

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите день недели: ");
+int i = Convert.ToInt32(Console.ReadLine());
+
+if (i == 1)Console.WriteLine("Понедельник");
+if (i == 2)Console.WriteLine("Вторник");
+if (i == 3)Console.WriteLine("Среда");
+if (i == 4)Console.WriteLine("Четверг");
+if (i == 5)Console.WriteLine("Пятница");
+if (i == 6)Console.WriteLine("Суббота");
+if (i == 7)Console.WriteLine("Воскресенье");
