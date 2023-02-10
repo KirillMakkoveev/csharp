692bdc7634c17a88ecbb6e89fd0a29b4a019d0b7
Kirill <Mrgrenn@gmail.com>
2023-02-10 21:53:55 +0300

    done task4

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,12 +1,4 @@
-﻿//Задача №5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
-//4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
-//2 -> " -2, -1, 0, 1, 2"
-
-Console.WriteLine("Введите число N: ");
+﻿Console.WriteLine("vvedite chislo trehznach:");
 int i = Convert.ToInt32(Console.ReadLine());
-int j = 0 - i;
-while (j <= i)
-{
-    Console.WriteLine(j);
-    j++;
-}
+int result = i % 10;
+System.Console.WriteLine(result);
--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 	456 -> 6
+// 	782 -> 2
+// 	918 -> 8
+
+
+Console.WriteLine("vvedite chislo trehznach:");
+int i = Convert.ToInt32(Console.ReadLine());
+int result = i % 10;
+System.Console.WriteLine(result);
