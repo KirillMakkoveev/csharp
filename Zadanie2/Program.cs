587c7434e63dab1552987f07c8eb46645bbc4d0a
Kirill <Mrgrenn@gmail.com>
2023-02-12 16:03:01 +0300

    Correct zadanie2

--- Zadanie2/Program.cs
+++ Zadanie2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("number1= ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("number2= ");
+int J = Convert.ToInt32(Console.ReadLine());
+if (i>J)
+{
+    System.Console.Write("Max= ");
+    System.Console.WriteLine(i);
+    }
+else 
+{
+    System.Console.Write("Max= ");
+    System.Console.Write(J);
+}
