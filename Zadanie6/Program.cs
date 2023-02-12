ea985443a7b2c18bf66ac65bc077c24bca713f2e
Kirill <Mrgrenn@gmail.com>
2023-02-12 18:12:45 +0300

    correct zadanie6

--- Zadanie6/Program.cs
+++ Zadanie6/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.WriteLine("vvedite chislo:");
+int i = Convert.ToInt32(Console.ReadLine());
+int result = i % 2;
+if (result == 1) Console.WriteLine("No");
+else Console.WriteLine("Yes");
