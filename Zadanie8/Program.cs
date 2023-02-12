e3a01360cd69b8f50f4e1853d8893102095186f5
Kirill <Mrgrenn@gmail.com>
2023-02-12 18:56:59 +0300

    correct zadanie8

---
+++ Zadanie8/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("vvedite chislo:");
+int Number = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i< Number; i++)
+{
+if (i%2 == 0) Console.Write(i); else Console.Write(",");
+}
