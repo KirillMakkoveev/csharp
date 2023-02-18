61d4144fe43afc76f844c4fed8fa2d115214a271
Kirill <Mrgrenn@gmail.com>
2023-02-18 13:21:38 +0300

    done Zadanie10

--- Zadanie10/Program.cs
+++ Zadanie10/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("vvedite trechznachnoe chislo:");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number <= 999 && Number >= 100)
+{
+Number = Number / 10;
+Number = Number % 10;
+Console.WriteLine(Number);
+}
+else Console.WriteLine("Error");
