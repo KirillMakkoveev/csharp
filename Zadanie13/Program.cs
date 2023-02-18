4261d82858d10f466ddc35552da2ed7ef5f46760
Kirill <Mrgrenn@gmail.com>
2023-02-18 13:57:23 +0300

    done Zadanie13

---
+++ Zadanie13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("vvedite chislo:");
+int Number = Convert.ToInt32(Console.ReadLine());
+if ( Number <= 99) Console.WriteLine("tretiei cifri net");
+else
+{
+    for (int i = 1; Number>=1000; i=i*10)
+Number = Number / 10;
+Number = Number % 10;
+Console.WriteLine(Number);
+}
