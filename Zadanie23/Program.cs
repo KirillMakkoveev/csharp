d6b9a5eb5464420f75e38ca7776a59f6229f5d20
Kirill <Mrgrenn@gmail.com>
2023-02-18 19:02:27 +0300

    Done Zadanie23

---
+++ Zadanie23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("vvedite chislo:");
+int Number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i<=Number)
+{
+    int Result = i*i*i;
+    Console.Write(Result + " ");
+    i = i+1;
+}
