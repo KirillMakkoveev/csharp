f7adc9c965bf44c7b01f1dd23f3fb8aa814a4d1e
Kirill <Mrgrenn@gmail.com>
2023-02-18 14:09:16 +0300

    done Zadanie15

--- Zadanie15/Program.cs
+++ Zadanie15/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("vvedite chislo:");
+int Number = Convert.ToInt32(Console.ReadLine());
+if ( Number <= 5 && Number >=1 ) Console.WriteLine("No");
+if ( Number >=6 && Number <=7 ) Console.WriteLine("Yes");
+if (Number >= 8) Console.WriteLine("Error");
